cc3b5e00eb75919c9c92c20741d7752d02896cc8
alexmarsian <42056329+alexmarsian@users.noreply.github.com>
2018-10-16 11:26:35 +1100

    Added file to test the domain of the EK model

---
+++ test_domain.m
@@ -0,0 +1,11 @@
+global GEOMETRY_PARAMETERS
+GEOMETRY_PARAMETERS = struct('a', 4.0, 'c', 3.0, 'r', 0.1, 'd', 0);
+figure(1)
+pdegplot(@domain2, 'EdgeLabels', 'on', 'SubdomainLabels', 'on')
+axis equal
+axis off
+
+figure(2)
+[p, e, t] = initmesh(@domain, 'MesherVersion', 'R2013a', 'Hmax', 0.1);
+pdeplot(p, e, t)
+axis equal
